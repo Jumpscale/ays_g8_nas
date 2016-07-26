26708e0c067243970dba4b676eb9191988f98f55
Christophe de Carvalho <zaibon@users.noreply.github.com>
2016-07-26 11:04:08 +0200

    fix user creation password

--- servicetemplates/user.os/actions.py
+++ servicetemplates/user.os/actions.py
@@ -4,4 +4,4 @@ from JumpScale import j
 class Actions(ActionsBaseMgmt):
 
     def install(self, service):
-        service.executor.cuisine.user.ensure(service.instance, service.hrd.getStr('password'))
+        service.executor.cuisine.user.ensure(service.instance, service.hrd.getStr('password'), encrypted_passwd=False)
